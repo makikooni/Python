06e944b437d8f73bb7866ac2f3315099198d039f
Weronika E. Falinska <w.e.falinska@gmail.com>
2024-02-09 16:48:09 +0000

    dance_update

--- dance_challenge/dance.py
+++ dance_challenge/dance.py
@@ -44,6 +44,15 @@ def draw():
         right.draw()
         left.draw()
         screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+    
+        if say_dance:
+            screen.draw.text("Dance!", color="black", topleft=(CENTER_X - 65, 150), fontsize=60)
+        if show_countdown:
+            screen.draw.text(str(count), color="black", topleft=(CENTER_X - 8, 150), fontsize=60)
+    else:
+        screen.clear()
+        screen.blit("stage", (0,0))
+        screen.draw.text("Score: " + str(score), color="black", topleft=(CENTER_X - 130,220), fontsize=60)
     return 
 
 def reset_dancer():
@@ -81,32 +90,109 @@ def update_dancer(move):
             clock.schedule(reset_dancer, 0.5)
     return 
 
+#function displaying computer generated moves
 def display_moves():
-    pass
-
+    global move_list, display_list, dance_length, say_dance, show_countdown, current_move
+    if display_list:
+        this_move = display_list[0]
+        display_list = display_list[1:]
+        if this_move == 0:
+            update_dancer(0)
+            #call the function in one second;
+            clock.schedule(display_moves, 1)
+        elif this_move == 1:
+            update_dancer(1)
+            clock.schedule(display_moves, 1)
+        elif this_move == 2:
+            update_dancer(1)
+            clock.schedule(display_moves, 2)
+        else:
+            update_dancer(3)
+            clock.schedule(display_moves, 1)
+    else:
+        say_dance = True
+        show_countdown = False
+    return 
+  
 def generate_moves():
-    pass
+    global move_list, dance_length, count, show_countdown, say_dance
+    count = 4
+    move_list = []
+    say_dance = False
+    for move in range(0, dance_length):
+        rand_move = randint(0, 3)
+        move_list.append(rand_move)
+        display_list.append(rand_move)
+    show_countdown = True
+    countdown()
+    return 
 
 def countdown():
-    pass
+    global count, game_over, show_countdown
+    if count > 1:
+        count = count - 1
+        clock.schedule(countdown, 1)
+    else:
+        show_countdown = False
+        display_moves()
+    return 
 
 def next_move():
-    pass
+    global dance_length, current_move, moves_complete
+    if current_move < dance_length - 1:
+        current_move = current_move + 1
+    else:
+        moves_complete = True
+    return 
 
 def on_key_up(key):
     global score, game_over, move_list, current_move
     if key ==keys.UP or key == keys.W:
         update_dancer(0)
+        if move_list[current_move] == 0:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
+            
     elif key == keys.RIGHT or key == keys.D:
         update_dancer(1)
+        if move_list[current_move] == 1:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
+            
     elif key == keys.DOWN or key == keys.S:
         update_dancer(2)
+        if move_list[current_move] == 2:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
+            
     elif key == keys.LEFT or key == keys.A:
         update_dancer(3)
+        if move_list[current_move] == 3:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
+            
     return 
 
+generate_moves()
+music.play("vanishing-horizon")
+
 def update():
-    pass
+    global game_over, current_move, moves_complete
+    if not game_over:
+        if moves_complete:
+            generate_moves()
+            moves_complete = False
+            current_move = 0
+        else:
+            music.stop()
 
 
 pgzrun.go()
